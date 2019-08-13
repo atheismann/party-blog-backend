6f29843cd2036f59ba7dcca640537d75217fe5cd
myDeveloperJourney <daniel@mydeveloperjourney.com>
2019-08-13 10:40:35 -0500

    adds latest changes

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-    res.render('index');
+    res.render('index', {title: "Welcome to Party Blog"});
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const path = require('path'); // allows us to generate dynamic paths
+const blogsController = require('./controllers/blogs');
 const app = express();
 const port = 3000;
 
-// set up a handler to respond to requests to the root path
+app.set('view engine', 'ejs');
+app.set(path.join(__dirname, 'views'));
 
-app.get('/', function(req, res) {
-    res.send('Hello World, you are on the homepage');
-});
+app.use('/blogs', blogsController);
 
 app.listen(port, function() {
     console.log(`Express is listening on port: ${port}`)
